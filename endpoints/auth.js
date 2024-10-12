c6206037467900d0869170083fccfedbf5439cfd
Kamil Ramocki <kamil.ramocki@gmail.com>
2024-10-12 14:15:49 +0200

    Added password encryption

--- endpoints/auth.js
+++ endpoints/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
 const authRouter = express.Router();
 
@@ -10,23 +11,25 @@ authRouter.post('/login', async (req, res) => {
         return res.json({ message: "User not found" });
     }
 
-    if (password === user.password) {
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
         res.json(user);
     } else {
         res.json({ message: "Authentication error" });
     }
 });
 
-
-
 authRouter.post('/register', async (req, res) => {
     const { email, password, name, surname, lookingForJob, cv } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
-        email, password, name, surname, lookingForJob, cv
+        email, password: hashedPassword, name, surname, lookingForJob, cv
     });
 
-    res.json(user)
+    res.json(user);
 });
 
 module.exports = { authRouter };
