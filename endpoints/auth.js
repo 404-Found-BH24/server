558bf5dded84af687ac8792ceb4d6e4cb481f604
Kamil Ramocki <kamil.ramocki@gmail.com>
2024-10-12 13:46:59 +0200

    Changed /auth/login to post, fixed error

--- endpoints/auth.js
+++ endpoints/auth.js
@@ -2,11 +2,22 @@ const express = require('express');
 const { User } = require('../models/user');
 const authRouter = express.Router();
 
-authRouter.get('/login', async (req, res) => {
-    const { email } = req.params;
-    const user = await User.findOne({ email });
-    res.json(user)
+authRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: { $regex: new RegExp(`^${email}$`, 'i') } });
+
+    if (!user) {
+        return res.json({ message: "User not found" });
+    }
+
+    if (password === user.password) {
+        res.json(user);
+    } else {
+        res.json({ message: "Authentication error" });
+    }
 });
+
+
 
 authRouter.post('/register', async (req, res) => {
     const { email, password, name, surname, lookingForJob, cv } = req.body;
